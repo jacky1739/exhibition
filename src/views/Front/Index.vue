722d5f7409fa413d82d039b0a5bb4c3806fb31a2
jacky <jacky123789456@gmail.com>
2021-08-05 11:19:45 +0800

    index.vue add wow

--- src/views/Front/Index.vue
+++ src/views/Front/Index.vue
@@ -3,10 +3,10 @@
     <div class="container d-flex align-items-center justify-content-center">
       <div class="row">
         <div class="col-lg-10 col-md-8">
-          <h2 class="h2 font-color-secondary mb-4 font-size-bold">台北當代藝術展</h2>
-          <p class="h3 font-color-secondary mb-4 font-size-Regular">01 JUN - 31 OCT</p>
+          <h2 class="h2 font-color-secondary mb-4 font-size-bold wow fadeInUp" data-wow-duration="1s" data-wow-delay="1s">台北當代藝術展</h2>
+          <p class="h3 font-color-secondary mb-4 font-size-Regular wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.5s">01 JUN - 31 OCT</p>
           <div class="d-grid gap-2">
-            <a class="btn btn-outline-secondary rounded-pill font-color-secondary rounded-pill border-white" href="#/about">關於我們</a>
+            <a class="btn btn-outline-secondary rounded-pill font-color-secondary rounded-pill border-white wow fadeInUp" data-wow-duration="1.5s" data-wow-delay="1.5s" href="#/about">關於我們</a>
           </div>
         </div>
       </div>
