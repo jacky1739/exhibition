51f8a9922997b10c62275b8b8b60265f25fc91d6
jacky123789456@gmail.com <jacky123789456@gmail.com>
2021-08-26 17:18:38 +0800

    在aboutus.vue新增div標籤

--- src/views/Front/AboutUs.vue
+++ src/views/Front/AboutUs.vue
@@ -1,25 +1,36 @@
 <template>
-  <section class="about-banner d-flex mb-5">
-    <div class="container d-flex align-items-center justify-content-center">
-      <div class="row">
-          <div class="col-md-12">
-            <div class="d-flex align-items-center justify-content-center">
-              <h1 class="text-secondary font-size-bold">關於我們</h1>
+  <div>
+    <section class="about-banner d-flex mb-5">
+      <div class="container d-flex align-items-center justify-content-center">
+        <div class="row">
+            <div class="col-md-12">
+              <div class="d-flex align-items-center justify-content-center">
+                <h1 class="text-secondary font-size-bold">關於我們</h1>
+              </div>
             </div>
-          </div>
+        </div>
       </div>
-    </div>
-  </section>
-  <section class="container mb-5">
-    <div class="row d-flex jutify-content-center">
-      <div class="col-md-12">
-        <p class="text-center text-secondary font-size-Regular">參觀展覽的美好時光</p>
-        <p class="text-center text-third font-size-light">2021 年 7 月，Jacky Hu 創立了 ARTS EXHIBITION ‧</p>
-        <p class="text-center text-third font-size-light">策辦各項臺灣現當代藝術展覽，傳播藝術訊息，鼓勵臺灣藝術學術與產業生產。</p>
-        <p class="text-center text-third font-size-light">促進全球交流、擴大首都之文化視野及提升國際能見度。</p>
-        <p class="text-center text-third font-size-light mb-4">蒐藏、保存、維護臺灣現的藝術，建構大家對於公共的記憶。</p>
-        <div class="w-100 about-content-img"></div>
+    </section>
+    <section class="container mb-5">
+      <div class="row d-flex jutify-content-center">
+        <div class="col-md-12">
+          <p class="text-center text-secondary font-size-Regular">參觀展覽的美好時光</p>
+          <p class="text-center text-third font-size-light">2021 年 7 月，Jacky Hu 創立了 ARTS EXHIBITION ‧</p>
+          <p class="text-center text-third font-size-light">策辦各項臺灣現當代藝術展覽，傳播藝術訊息，鼓勵臺灣藝術學術與產業生產。</p>
+          <p class="text-center text-third font-size-light">促進全球交流、擴大首都之文化視野及提升國際能見度。</p>
+          <p class="text-center text-third font-size-light mb-4">蒐藏、保存、維護臺灣現的藝術，建構大家對於公共的記憶。</p>
+          <div class="w-100 about-content-img"></div>
+        </div>
       </div>
-    </div>
-  </section>
+    </section>
+  </div>
 </template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  }
+}
+</script>
